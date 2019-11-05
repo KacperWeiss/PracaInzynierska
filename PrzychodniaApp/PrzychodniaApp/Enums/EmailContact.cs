7d3a533629b457053a6612efc29274346e9ee184
Kacper Weiss <kacper.weiss@gmail.com>
2019-11-05 21:42:02 +0100

    Added enum for email contact

---
+++ PrzychodniaApp/PrzychodniaApp/Enums/EmailContact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrzychodniaApp.Enums
+{
+    /// <summary>
+    /// It might be used later for automatic email related purpose.
+    /// </summary>
+    public enum EmailContact
+    {
+        Full,
+        ConfirmationsAndReminders,
+        OnlyForConfirmations,
+        No
+    }
+}
