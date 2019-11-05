7efacd69fdf4eff7b771eb62b63f00bdebed40da
Kacper Weiss <kacper.weiss@gmail.com>
2019-11-05 21:16:09 +0100

    Added enums regarding patients

---
+++ PrzychodniaApp/PrzychodniaApp/Enums/Pronouns.cs
@@ -0,0 +1,11 @@
+﻿namespace PrzychodniaApp.Enums
+{
+    /// <summary>
+    /// Pronouns to distiguish male from female. It might be used later for automatic email related purpose.
+    /// </summary>
+    public enum Pronouns
+    {
+        MR,
+        MRS
+    }
+}
