43e3d3ebfb919fb3b1642d0d80a8de339b795db9
Kacper Weiss <kacper.weiss@gmail.com>
2019-11-19 21:47:52 +0100

    Added Treatment db model

---
+++ PrzychodniaApp/PrzychodniaApp/DataBaseStuff/Models/Treatment.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrzychodniaApp.DataBaseStuff.Models
+{
+    /// <summary>
+    /// Database model describing treatment provided to the patient.
+    /// </summary>
+    public class Treatment : Entity
+    {
+        /// <summary>
+        /// Illness' name
+        /// </summary>
+        [Required]
+        public string IllnessName { get; set; }
+
+        /// <summary>
+        /// Short description of all symptoms.
+        /// </summary>
+        [Required]
+        [MaxLength(500)]
+        public string SymptomsDescription { get; set; }
+
+        /// <summary>
+        /// Time of the visit
+        /// </summary>
+        [Required]
+        public DateTime VisitDate { get; set; }
+
+        /// <summary>
+        /// List of all medications prescribed to the patient
+        /// </summary>
+        public IList<PrescribedMedications> Prescription { get; set; }
+    }
+}
