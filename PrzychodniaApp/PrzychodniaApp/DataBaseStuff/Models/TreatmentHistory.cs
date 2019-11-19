42d2d6b90c9983f04ca027809e4ad999d2a90071
Kacper Weiss <kacper.weiss@gmail.com>
2019-11-19 21:33:07 +0100

    TreatmentHistory db model added.

---
+++ PrzychodniaApp/PrzychodniaApp/DataBaseStuff/Models/TreatmentHistory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrzychodniaApp.DataBaseStuff.Models
+{
+    /// <summary>
+    /// Database model describing patient's treatment history.
+    /// </summary>
+    public class TreatmentHistory : Entity
+    {
+        /// <summary>
+        /// Provides information about last time patient had visited.
+        /// </summary>
+        [Required]
+        public DateTime LastVisit { get; set; }
+
+        /// <summary>
+        /// Lists treatments and illnesses that patient underwent in the past.
+        /// </summary>
+        public IList<Treatment> Treatments { get; set; }
+
+        /// <summary>
+        /// Lists vaccinations that patient underwent in the past.
+        /// </summary>
+        public IList<Vaccination> Vaccinations { get; set; } 
+    }
+}
