b5bcffb65916de4a6bf037679432ed1d6c7f4d82
Kacper Weiss <kacper.weiss@gmail.com>
2019-11-19 22:10:08 +0100

    Added Vaccination db model.

---
+++ PrzychodniaApp/PrzychodniaApp/DataBaseStuff/Models/Vaccination.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrzychodniaApp.DataBaseStuff.Models
+{
+    /// <summary>
+    /// Database model describing each vaccination that was given to the patient.
+    /// </summary>
+    public class Vaccination : Entity
+    {
+        /// <summary>
+        /// Name of the vaccine
+        /// </summary>
+        [Required]
+        public string VaccinesName { get; set; }
+
+        /// <summary>
+        /// Enum describing if vaccination is optional, obligatory before reaching certain age, or obligatory and patient is already vaccinated.
+        /// </summary>
+        [Required]
+        public VaccineStatus VaccineStatus { get; set; }
+
+        /// <summary>
+        /// Describes when patient was vaccineted
+        /// </summary>
+        public DateTime? VaccinationDate { get; set; }
+
+        /// <summary>
+        /// By this date patient should be vaccinated if vaccine is obligatory.
+        /// </summary>
+        public DateTime? ObligatoryBy { get; set; }
+    }
+}
