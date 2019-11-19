83ab1787c6fbbccccd30f487f7e802a48a36fb8a
Kacper Weiss <kacper.weiss@gmail.com>
2019-11-19 21:53:34 +0100

    Added PrescribedMedications db model

---
+++ PrzychodniaApp/PrzychodniaApp/DataBaseStuff/Models/PrescribedMedications.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrzychodniaApp.DataBaseStuff.Models
+{
+    /// <summary>
+    /// Database model describing prescribed medications.
+    /// </summary>
+    public class PrescribedMedications : Entity
+    {
+        /// <summary>
+        /// Name of the medicine
+        /// </summary>
+        [Required]
+        public string MedicinesName { get; set; }
+
+        /// <summary>
+        /// Describes dosage for specific medicine
+        /// </summary>
+        [Required]
+        public string Dosage { get; set; } 
+    }
+}
