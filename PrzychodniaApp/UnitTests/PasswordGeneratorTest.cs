b7655890afb018f1df300ef952e9b26d1bcb1939
Kacper Weiss <kacper.weiss@gmail.com>
2020-02-06 20:57:49 +0100

    Added minimal Unit Tests

---
+++ PrzychodniaApp/UnitTests/PasswordGeneratorTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrzychodniaApp.Logics;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class PasswordGeneratorTest
+    {
+        [TestMethod]
+        public void ReturnsStringPassword()
+        {
+            // Act
+            var testPassword = PasswordGenerator.GetRandomPassword();
+
+            // Assert
+            Assert.IsInstanceOfType(testPassword, typeof(string), "Zwrócone hasło nie jest typu string.");
+        }
+
+        [TestMethod]
+        public void ReturnsPasswordOfSpecifiedLength()
+        {
+            // Arrange
+            int requiredLength = 20;
+
+            // Act
+            var testPassword = PasswordGenerator.GetRandomPassword();
+            
+            // Assert
+            Assert.AreEqual(testPassword.Length, requiredLength);
+        }
+        // (...)
+    }
+}
